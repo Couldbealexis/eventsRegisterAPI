c3280481ab223352db6fadf3ae7811c9cbc1a31b
Cesar Alexis Gutierrez Gonzalez <couldbealexis@icloud.com>
2019-08-27 18:56:21 -0500

    event attendees routes working

--- controllers/eventAttendees.controller.js
+++ controllers/eventAttendees.controller.js
@@ -7,52 +7,51 @@ const {
 } = require('../services/response');
 
 exports.create = (req, res) => {
-  let body = req.body;
-  // body = JSON.parse(body.attendees);
-  // console.log(body.attendees);
-  // body = Array.from(body.attendees);
-  console.log(body);
-  // console.log("keys", Object.keys(body).length);
-//   (Object.keys(body)).forEach(element => {
-//       console.log(element);
-//       console.log(body[element]);
-//   });
-
-  res.send(body);
-//   const detail = EventDetails.build(body);
-//   detail.save().then(() => {
-//     sendResponse(res, 'true', '200', detail);
-//   }).catch((err) => {
-//     sendResponse(res, 'false', '400', {}, 'No se ha podido crear el detalle de evento', err.message);
-//   });
+  const body = req.body;
+  const attendees = JSON.parse(
+    body.attendees,
+  );
+  (Object.keys(attendees)).forEach((key) => {
+    const attendee = EventAttendees.build(attendees[key]);
+    attendee.event = body.event;
+    attendee.save();
+  });
+  sendResponse(res, 'true', '200', {});
 };
 
 const detail = (res, id) => {
-  EventDetails.findOne({
+  EventAttendees.findOne({
     where: {
       id,
     },
     attributes: {
       exclude: ['createdAt', 'updatedAt'],
     },
-  }).then((type) => {
-    if (!type) return sendResponse(res, 'false', '404', {}, 'No se encontro el tipo de evento', 'Type not found');
-    return sendResponse(res, 'true', '200', type);
+  }).then((attendee) => {
+    if (!attendee) return sendResponse(res, 'false', '404', {}, 'No se encontro el asistente', 'Attendee not found');
+    return sendResponse(res, 'true', '200', attendee);
   }).catch((err) => {
-    return sendResponse(res, 'false', '404', {}, 'No se encontro el tipo de evento', err.message);
+    return sendResponse(res, 'false', '400', {}, 'No se encontro el asistente', err.message);
   });
 };
 
 exports.findAll = (req, res) => {
-  return detail(res, req.params.id);
+  EventAttendees.findAndCountAll({
+    where: { event: req.params.event },
+  }).then((attendees) => {
+    if (!attendees) return sendResponse(res, 'false', '404', {}, 'No se encontraron asistentes', 'Not found');
+    return sendResponse(res, 'true', '200', attendees);
+  }).catch((err) => {
+    return sendResponse(res, 'false', '400', {}, 'Error al traer los asistentes', err.message);
+  });
 };
 
 exports.updateOne = (req, res) => {
   const body = req.body;
-  EventDetails.update(body, {
+  EventAttendees.update(body, {
     where: { id: req.params.id },
-  }).then((type) => {
-    if (!type) return sendResponse(res, '404', {}, 'No se pudo actualizar', 'EventType error at update');
+  }).then((attendee) => {
+    if (!attendee) return sendResponse(res, '404', {}, 'No se pudo actualizar', 'EventAttendee error at update');
     return detail(res, req.params.id);
   }).catch((err) => {
     return sendResponse(res, 'false', '400', {}, 'tipo de evento no encontrado', err.message);
